aad4965717e1e4047a8fe57333cd13f3ae9d0584
Mark Paluch <mpaluch@pivotal.io>
2019-02-19 10:07:57 +0100

    Add actuator support.

---
+++ spring-boot-actuator-r2dbc/src/main/java/org/springframework/boot/actuate/r2dbc/ConnectionFactoryHealthIndicator.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.actuate.r2dbc;
+
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.Row;
+import io.r2dbc.spi.RowMetadata;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.boot.actuate.health.AbstractReactiveHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+/**
+ * Health indicator for R2DBC ConnectionFactory.
+ *
+ * @author Mark Paluch
+ */
+public class ConnectionFactoryHealthIndicator extends AbstractReactiveHealthIndicator {
+
+	private static final String DEFAULT_QUERY = "SELECT 1";
+
+	private final ConnectionFactory connectionFactory;
+
+	private String query;
+
+	/**
+	 * Create a new {@link ConnectionFactoryHealthIndicator} using the specified
+	 * {@link ConnectionFactory}.
+	 * @param connectionFactory the connection factory
+	 */
+	public ConnectionFactoryHealthIndicator(ConnectionFactory connectionFactory) {
+		this(connectionFactory, null);
+	}
+
+	/**
+	 * Create a new {@link ConnectionFactoryHealthIndicator} using the specified
+	 * {@link ConnectionFactory} and validation query.
+	 * @param connectionFactory the connection factory
+	 * @param query the validation query to use (can be {@code null})
+	 */
+	public ConnectionFactoryHealthIndicator(ConnectionFactory connectionFactory, String query) {
+
+		Assert.notNull(connectionFactory, "ConnectionFactory must not be null");
+		this.connectionFactory = connectionFactory;
+		this.query = query;
+	}
+
+	@Override
+	protected Mono<Health> doHealthCheck(Health.Builder builder) {
+		String product = getProduct();
+		builder.up().withDetail("database", product);
+		String validationQuery = getValidationQuery(product);
+		if (StringUtils.hasText(validationQuery)) {
+			builder.withDetail("validationQuery", validationQuery);
+			Mono<Object> result = runValidationQuery(validationQuery);
+			return result.map(it -> {
+				builder.withDetail("result", it);
+				return builder.build();
+			}).defaultIfEmpty(builder.build())
+					.onErrorResume(Exception.class, (e) -> Mono
+							.just(builder.down(e).build()));
+		}
+		return Mono.just(builder.build());
+	}
+
+	private Mono<Object> runValidationQuery(String validationQuery) {
+		return Mono
+				.usingWhen(this.connectionFactory.create(), conn -> {
+					return Flux.from(conn.createStatement(validationQuery).execute())
+							.flatMap(it -> it.map(this::extractResult)).next();
+
+				}, Connection::close, Connection::close, Connection::close);
+	}
+
+	private Object extractResult(Row row, RowMetadata metadata) {
+		return row.get(metadata.getColumnMetadatas().iterator()
+				.next()
+				.getName());
+	}
+
+	private String getProduct() {
+		return connectionFactory.getMetadata().getName();
+	}
+
+	protected String getValidationQuery(String product) {
+		String query = this.query;
+		if (!StringUtils.hasText(query)) {
+			DatabaseDriver specific = DatabaseDriver.fromProductName(product);
+			query = specific.getValidationQuery();
+		}
+		if (!StringUtils.hasText(query)) {
+			query = DEFAULT_QUERY;
+		}
+		return query;
+	}
+
+	/**
+	 * Set a specific validation query to use to validate a connection. If none is set, a
+	 * default validation query is used.
+	 * @param query the query
+	 */
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	/**
+	 * Return the validation query or {@code null}.
+	 * @return the query
+	 */
+	public String getQuery() {
+		return this.query;
+	}
+}
---
+++ spring-boot-actuator-r2dbc/src/test/java/org/springframework/boot/actuate/r2dbc/ConnectionFactoryHealthIndicatorTests.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.actuate.r2dbc;
+
+
+import java.util.Random;
+
+import io.r2dbc.client.R2dbc;
+import io.r2dbc.h2.H2ConnectionConfiguration;
+import io.r2dbc.h2.H2ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.Result;
+import org.junit.Before;
+import org.junit.Test;
+import reactor.test.StepVerifier;
+
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.jdbc.DatabaseDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.entry;
+
+/**
+ * Tests for {@link ConnectionFactoryHealthIndicator}.
+ *
+ * @author Mark Paluch
+ */
+public class ConnectionFactoryHealthIndicatorTests {
+
+	private ConnectionFactoryHealthIndicator healthIndicator;
+
+	private ConnectionFactory connectionFactory;
+
+	@Before
+	public void init() {
+		connectionFactory = new H2ConnectionFactory(H2ConnectionConfiguration.builder()
+				.inMemory("db-" + new Random().nextInt()).option("DB_CLOSE_DELAY=-1")
+				.build());
+	}
+
+	@Test
+	public void healthIndicatorWithDefaultSettings() {
+		this.healthIndicator = new ConnectionFactoryHealthIndicator(this.connectionFactory);
+		this.healthIndicator.health().as(StepVerifier::create).assertNext((actual) -> {
+			assertThat(actual.getStatus()).isEqualTo(Status.UP);
+			assertThat(actual.getDetails()).containsOnly(
+					entry("database", "H2"), entry("result", 1),
+					entry("validationQuery", DatabaseDriver.H2.getValidationQuery()));
+		}).verifyComplete();
+	}
+
+	@Test
+	public void healthIndicatorWithCustomValidationQuery() {
+		String customValidationQuery = "SELECT COUNT(*) from FOO";
+		new R2dbc(connectionFactory).useHandle(h -> h
+				.createQuery("CREATE TABLE FOO (id INTEGER IDENTITY PRIMARY KEY)")
+				.mapResult(Result::getRowsUpdated)).as(StepVerifier::create)
+				.expectNextCount(0).verifyComplete();
+		this.healthIndicator = new ConnectionFactoryHealthIndicator(this.connectionFactory, customValidationQuery);
+		this.healthIndicator.health().as(StepVerifier::create).assertNext((actual) -> {
+			assertThat(actual.getStatus()).isEqualTo(Status.UP);
+			assertThat(actual.getDetails()).containsOnly(
+					entry("database", "H2"), entry("result", 0L),
+					entry("validationQuery", customValidationQuery));
+		}).verifyComplete();
+	}
+
+	@Test
+	public void healthIndicatorWithInvalidValidationQuery() {
+		String invalidValidationQuery = "SELECT COUNT(*) from BAR";
+		this.healthIndicator = new ConnectionFactoryHealthIndicator(this.connectionFactory, invalidValidationQuery);
+		this.healthIndicator.health().as(StepVerifier::create).assertNext((actual) -> {
+			assertThat(actual.getStatus()).isEqualTo(Status.DOWN);
+			assertThat(actual.getDetails()).contains(
+					entry("database", "H2"), entry("validationQuery", invalidValidationQuery));
+			assertThat(actual.getDetails()).containsOnlyKeys("database", "error",
+					"validationQuery");
+		}).verifyComplete();
+	}
+}
